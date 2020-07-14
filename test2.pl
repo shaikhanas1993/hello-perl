af3692a032c68e421178d176b93619d9ae625f96
anas <anas.shaikh@sundaymobility.com>
2020-07-14 15:12:34 +0530

    elegancy in when

--- test2.pl
+++ test2.pl
@@ -5,24 +5,13 @@ use feature qw(say  switch);
 use Data::Printer;
 use integer;
 
-my ($color,$code);
+say "Enter something";
+chomp(my $input = <>);
 
-say "please enter a color ::\n";
-
-$color =  <STDIN>;
-chomp($color);
-$color = uc($color);
-
-given($color){
-    when($_ eq 'RED') {$code = 'sfsfa'}
-    when($_  eq 'GREEN') {$code = 'afsfa'}
-    default{$code = ''}
-}
-
-if($code ne '')
-{
-    say "code is $code";
-}else
-{
-    say "code is empty";
+print do{
+    given($input){
+        'all numbers' when /\d/;
+        'text' when /[a-zA-z]/;
+        default{'defaut value________';}
+    }
 }
