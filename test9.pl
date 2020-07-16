b4098a1a30e6e07a46d9e6d474db255b8586ec02
anas <anas.shaikh@sundaymobility.com>
2020-07-16 19:23:36 +0530

    learn about label and nested loops in perl

--- test9.pl
+++ test9.pl
@@ -4,7 +4,7 @@ use experimental qw(smartmatch lexical_subs);
 use feature qw(say switch);
 use integer;
 use Data::Printer;
-
+=begin
 my($key,$value);
 
 my %dic = ("apple" => 1, "orange" => 2, "banana" => 3);
@@ -26,3 +26,44 @@ if(defined  $value && $value ne ''){
 }else{
     say "value not found";    
 }
+=end
+=cut
+
+
+my ($key,$value);
+
+my %h = ("apple" => 1, "banana" => 2, "carrot" => 3);
+
+say "Plese enter a key\n";
+
+OUTER:
+while(<STDIN>){
+    $key = $_;
+    chomp($key);
+    $key = lc($key);
+
+    INNER:
+          foreach(keys %h){
+              if($_ eq $key){
+                  $value = $h{$_};
+                  last OUTER;
+              }
+          }
+
+}
+
+
+    say "value is :: $value\n";
+
+
+
+
+
+
+
+
+
+
+
+
+
