a298f68f06188ab9080b6dd2fc0cedeaed6cb107
anas <anas.shaikh@sundaymobility.com>
2020-07-18 16:53:52 +0530

    learn about references in perl

---
+++ test11.pl
@@ -0,0 +1,15 @@
+use strict;
+use warnings;
+use Data::Printer;
+
+# my $arr = [1..9,[10..20]];
+# p $arr;
+
+# my $hash = {"anas" =>1 , "jarrar" => {"data" => 1}};
+# print $hash;
+# print $hash->{"jarrar"};
+
+my $dummy->{"building"}->{"roomno"} = 10;
+print $dummy . "\n";
+print $dummy->{"building"} . "\n";
+print $dummy->{"building"}->{"roomno"};
