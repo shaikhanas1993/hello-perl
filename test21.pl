55d13a4afb5cd773ffb52912ff6416e0754bd847
anas <anas.shaikh@sundaymobility.com>
2020-07-23 12:16:58 +0530

    learn about reading files in perl

--- test21.pl
+++ test21.pl
@@ -1,6 +1,30 @@
 use strict;
 use warnings;
 
+
+# open(FH ,'<' , 'dummy.txt') or  die $!;
+# while(<FH>){
+#    print $_;
+# }
+# close(FH);
+
+# usage of diamond operator to run until end of file is provided 
 while(<>){
    print $_;
 }
+
+
+
+# use strict;
+# use warnings;
+
+# open(FH,"<",'dummy.txt') or die $!;
+# while(<FH>){
+#    print $_;
+# }
+# close(FH);
+
+
+# while(<>){
+#    print $_;
+# }
