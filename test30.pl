d71ddc975634c933875057c1bdae687f98390f40
anas <anas.shaikh@sundaymobility.com>
2020-07-26 17:31:05 +0530

    learn about perl matching operators

--- test30.pl
+++ test30.pl
@@ -3,23 +3,44 @@ use strict;
 use warnings;
 
 
-my @words = (
-	   'Perl',
-	   'regular expression',
-	   'is',
-	   'a very powerul',
-	   'feature'
-	);
 
-#matches a regular  expression
-for(@words){
-    print $_ . "\n"  if($_ =~ /er/);
-}
 
-#does not matches a regular  expression
-for(@words){
-    print $_ . "\n"  if($_ !~ /er/);
-}
+# my @html = (
+#     '<p>',
+# 	'html fragement',
+# 	'</p>',
+# 	'<br>',
+# 	'<span>This is a span</span>'
+# );
+# ##uses backslash
+# # foreach(@html){
+# #     print $_ . "\n" if $_ =~ /\//;
+# # }
+
+# #use match operator
+# foreach(@html){
+#     print $_ . "\n" if $_ =~ m"/";
+# }
+
+
+
+# my @words = (
+# 	   'Perl',
+# 	   'regular expression',
+# 	   'is',
+# 	   'a very powerul',
+# 	   'feature'
+# 	);
+
+# #matches a regular  expression
+# for(@words){
+#     print $_ . "\n"  if($_ =~ /er/);
+# }
+
+# #does not matches a regular  expression
+# for(@words){
+#     print $_ . "\n"  if($_ !~ /er/);
+# }
 
 
 # my $string = "Perl regular expression is powerful";
