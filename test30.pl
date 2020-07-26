9059ef9766b7e894e18555f80f84e8c5b4486685
anas <anas.shaikh@sundaymobility.com>
2020-07-26 17:25:53 +0530

    learn about perl binding operators

--- test30.pl
+++ test30.pl
@@ -3,10 +3,29 @@ use strict;
 use warnings;
 
 
-my $string = "Perl regular expression is powerful";
-if($string =~ /ul/)
-{
-    print "match found";
-}else{
-    print "match not found";
+my @words = (
+	   'Perl',
+	   'regular expression',
+	   'is',
+	   'a very powerul',
+	   'feature'
+	);
+
+#matches a regular  expression
+for(@words){
+    print $_ . "\n"  if($_ =~ /er/);
 }
+
+#does not matches a regular  expression
+for(@words){
+    print $_ . "\n"  if($_ !~ /er/);
+}
+
+
+# my $string = "Perl regular expression is powerful";
+# if($string =~ /ul/)
+# {
+#     print "match found";
+# }else{
+#     print "match not found";
+# }
